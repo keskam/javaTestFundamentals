4263d79d556c16836b32c834f07d5c8d5c9528e5
keskam <30647981+keskam@users.noreply.github.com>
2019-11-19 21:17:33 +0200

    Mockito library example

---
+++ JavaTest/src/main/Calculator.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Calculator {
+
+    public int add(int variable1, int variable2) {
+        return variable1 + variable2;
+    }
+
+    public int sub(int variable1, int variable2) {
+        return variable1 - variable2;
+    }
+}
---
+++ JavaTest/src/test/CalculatorTest.java
@@ -0,0 +1,18 @@
+import main.Calculator;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
+public class CalculatorTest {
+
+    @Test
+    public void addTest() {
+
+        Calculator calculator = Mockito.mock(Calculator.class);
+        when(calculator.add(3, 5)).thenReturn(8);
+        int result = calculator.add(3, 9);
+
+        assert result == 8;
+    }
+}
